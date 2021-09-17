a9778020f652354743582604aa5c9a888f01cd31
pedreviljoen <pedre.viljoen@lifeq.com>
2021-09-17 10:16:21 +0200

    added: weather comments challenge

--- exercises/go/weather-forecast/weather.go
+++ exercises/go/weather-forecast/weather.go
@@ -1,10 +1,14 @@
+// Package weather provides logging functionality for current conditions
 package weather
 
 var (
+	// CurrentCondition represents the current weather conditions
 	CurrentCondition string
-	CurrentLocation  string
+	// CurrentCondition represents the current location for the weather conditions
+	CurrentLocation string
 )
 
+// Log returns a string which describes the current weather conditions at the current location
 func Log(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
