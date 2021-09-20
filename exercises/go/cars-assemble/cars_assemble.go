5847f3b3498d36300d003044ddc2ef87edd89318
pedreviljoen <pedre.viljoen@lifeq.com>
2021-09-20 09:25:27 +0200

    added: added cars-complete

---
+++ exercises/go/cars-assemble/cars_assemble.go
@@ -0,0 +1,19 @@
+package cars
+
+// CalculateProductionRatePerHour for the assembly line, taking into account
+// its success rate
+func CalculateProductionRatePerHour(speed int) float64 {
+	panic("not implemented")
+}
+
+// CalculateProductionRatePerMinute describes how many working items are
+// produced by the assembly line every minute
+func CalculateProductionRatePerMinute(speed int) int {
+	panic("not implemented")
+}
+
+// successRate is used to calculate the ratio of an item being created without
+// error for a given speed
+func successRate(speed int) float64 {
+	panic("not implemented")
+}
