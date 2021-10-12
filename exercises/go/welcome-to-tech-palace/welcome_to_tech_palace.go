cd236055d2631b26cf7711f40d8bffe6c5875d21
pedreviljoen <pedre.viljoen@lifeq.com>
2021-10-12 10:25:30 +0200

    added: completed strings exercise

--- exercises/go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ exercises/go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,30 @@
 package techpalace
 
+import (
+	"fmt"
+	"strings"
+)
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return fmt.Sprintf("Welcome to the Tech Palace, %s", strings.ToUpper(customer))
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	// create stars string literal
+	stars := ""
+	for i := 0; i < numStarsPerLine; i++ {
+		stars = stars + "*"
+	}
+
+	// return string message
+	return stars + "\n" + welcomeMsg + "\n" + stars
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	noStars := strings.ReplaceAll(oldMsg, "*", "")
+
+	return strings.TrimSpace(noStars)
 }
